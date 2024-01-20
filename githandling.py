732ff6f3cd8bd07c5107a37634fd17435c8e1256
Tamim Burgstaller <tamim.burgstaller@student.tugraz.at>
2024-01-20 22:45:34 +0100

    Add branching support

--- githandling.py
+++ githandling.py
@@ -4,24 +4,33 @@ from git import Repo
 
 
 class GitWrapper:
-    def __init__(self, repo_path, bare_repo_path):
+    def __init__(self, repo_path, bare_repo_path, mimic_cli_git_commit=False):
+        self.mimic_cli_git_commit = mimic_cli_git_commit
         self.repo_path = repo_path
         self.bare_repo_path = bare_repo_path
 
-        self.repo = Repo.init(self.repo_path)
-        self.bare_repo = Repo.init(self.bare_repo_path, bare=True)
-        assert not self.repo.bare
+        self.bare_repo: Repo = Repo.init(self.bare_repo_path, bare=True)
         assert self.bare_repo.bare
+        # self.repo = Repo.init(self.repo_path)
+        self.repo: Repo = self.bare_repo.clone(self.repo_path)
+        assert not self.repo.bare
         # self.repo.create_remote('origin', self.bare_repo_path)
 
         self.commit_number = 0
         self.tag_number = 0
+        self.branches = {'master': self.repo.active_branch}
+
+    def current_branch(self):
+        return "BRANCH " + str(self.repo.active_branch)
 
     def untracked(self):
         return "UNTRACKED " + str(len(self.repo.untracked_files))
 
     def is_dirty(self):
         return "DIRTY" if self.repo.is_dirty() else "NOT_DIRTY"
+
+    def modified(self):
+        return str(len(self.repo.index.diff(None)))
 
     def add(self, file: str):
         return self.add_all([file])
@@ -34,6 +43,8 @@ class GitWrapper:
             return "ADD_FAIL"
 
     def commit(self):
+        if self.mimic_cli_git_commit and not self.repo.is_dirty():
+            return "COMMIT_FAIL"
         try:
             self.repo.index.commit("Commit number " + str(self.commit_number))
             self.commit_number += 1
@@ -48,6 +59,48 @@ class GitWrapper:
             return "TAG_SUCCESS"
         except Exception:
             return "TAG_FAIL"
+
+    def fetch(self):
+        try:
+            origin = self.repo.remotes.origin
+            origin.fetch()
+            return "FETCH_SUCCESS"
+        except Exception:
+            return "FETCH_FAIL"
+
+    def pull(self):
+        try:
+            origin = self.repo.remotes.origin
+            origin.pull()
+            return "PULL_SUCCESS"
+        except Exception:
+            return "PULL_FAIL"
+
+    def push(self):
+        try:
+            origin = self.repo.remotes.origin
+            origin.push().raise_if_error()
+            return "PUSH_SUCCESS"
+        except Exception:
+            return "PUSH_FAIL"
+
+    def create_branch(self, branch_name):
+        try:
+            new_branch = self.repo.create_head(branch_name, "HEAD")
+            self.repo.head.reference = new_branch
+            self.repo.head.reset(index=True, working_tree=True)
+            self.branches[branch_name] = new_branch
+            return "BRANCH_SUCCESS"
+        except Exception:
+            return "BRANCH_FAIL"
+
+    def checkout(self, branch_name):
+        try:
+            self.repo.head.reference = self.branches[branch_name]
+            self.repo.head.reset(index=True, working_tree=True)
+            return "CHECKOUT_SUCCESS"
+        except Exception:
+            return "CHECKOUT_FAIL"
 
     def destroy(self):
         os.system('rm -rf ' + self.repo_path)
--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import os
 import random
 
 import aalpy
-import git
 from aalpy.base import SUL
-from aalpy.oracles import RandomWalkEqOracle, WMethodEqOracle
+from aalpy.oracles import RandomWordEqOracle, RandomWalkEqOracle, WMethodEqOracle
 from aalpy.learning_algs import run_Lstar, run_KV
-from aalpy.utils import visualize_automaton
-from git import Repo, Remote
+from aalpy.utils import visualize_automaton, compare_automata
 
 from filehandling import *
 from githandling import *
@@ -16,18 +14,21 @@ from explorer import print_automaton_details, log_automaton_details
 repo_path: str = '/tmp/repo'
 bare_repo_path: str = '/tmp/barerepo.git'
 
+# Comment these in or out to enable/disable them
 input_alphabet: list = [
     # File operations
     'create_f0',
     # 'create_f1',
-    'change_f0',
+    # 'change_f0',
     # 'change_f1',
-    'delete_f0',
+    # 'delete_f0',
     # 'delete_f1',
 
     # Git status checks
+    'branch',
     # 'untracked',
-    'dirty',
+    # 'dirty',
+    # 'modified'
 
     # Git commands
     # 'add_all',
@@ -37,7 +38,10 @@ input_alphabet: list = [
     # 'fetch',
     # 'pull',
     # 'push',
-    # 'tag'
+    # 'tag',
+    'create_branch',
+    'checkout_master',
+    'checkout_branch'
 ]
 
 
@@ -53,8 +57,15 @@ def get_random_garbage() -> str:
     return str(random.choices(range(ord('A'), ord('Z')), k=random.randint(5, 25)))
 
 
+def clean_up():
+    if os.path.exists(repo_path):
+        os.system('rm -rf ' + repo_path)
+    if os.path.exists(bare_repo_path):
+        os.system('rm -rf ' + bare_repo_path)
+
+
 class GitSUL(SUL):
-    def __init__(self, path_to_repo, path_to_bare_repo):
+    def __init__(self, path_to_repo, path_to_bare_repo, mimic_cli_git_commit=False):
         super().__init__()
         self.repo_path: str = path_to_repo
         self.bare_repo_path: str = path_to_bare_repo
@@ -63,10 +74,13 @@ class GitSUL(SUL):
 
         self.filenames: list[str] = ['file0.txt', 'file1.txt']
 
+        self.branches = ['master', 'other_branch']
+
         self.git: GitWrapper | None = None
+        self.mimic_cli_git_commit = mimic_cli_git_commit
 
     def pre(self):
-        self.git = GitWrapper(self.repo_path, self.bare_repo_path)
+        self.git = GitWrapper(self.repo_path, self.bare_repo_path, self.mimic_cli_git_commit)
 
     def post(self):
         if self.git is not None:
@@ -90,10 +104,14 @@ class GitSUL(SUL):
                 return delete_file(self.repo_path + '/' + self.filenames[0])
 
             # Git status checks
+            case 'branch':
+                return self.git.current_branch()
             case 'untracked':
                 return self.git.untracked()
             case 'dirty':
                 return self.git.is_dirty()
+            case 'modified':
+                return self.git.modified()
 
             # Git commands
             case 'add_all':
@@ -104,10 +122,20 @@ class GitSUL(SUL):
                 return self.git.add(self.filenames[1])
             case 'commit':
                 return self.git.commit()
-            # case 'fetch':
-            #     return self.repo.remotes['origin'].fetch()  # Alternative syntax: self.repo.remotes.origin.fetch()
             case 'tag':
                 return self.git.tag()
+            case 'fetch':
+                return self.git.fetch()
+            case 'pull':
+                return self.git.pull()
+            case 'push':
+                return self.git.push()
+            case 'create_branch':
+                return self.git.create_branch(self.branches[1])
+            case 'checkout_branch':
+                return self.git.checkout(self.branches[1])
+            case 'checkout_master':
+                return self.git.checkout(self.branches[0])
             case _:
                 if letter in input_alphabet:
                     raise RuntimeError("Something went terribly wrong! (Letter: " + letter + ")")
@@ -121,13 +149,31 @@ def main():
     global bare_repo_path
 
     git_sul = GitSUL(repo_path, bare_repo_path)
-    eq_oracle = RandomWalkEqOracle(input_alphabet, git_sul)
+    eq_oracle = RandomWordEqOracle(input_alphabet, git_sul, num_walks=500)
     mealy = run_Lstar(input_alphabet, git_sul, eq_oracle, automaton_type='mealy', cache_and_non_det_check=True)
     # mealy = run_KV(input_alphabet, git_sul, eq_oracle, automaton_type='mealy', cache_and_non_det_check=True)
 
     visualize_automaton(mealy, 'git-model.pdf')
     print_automaton_details(mealy)
     log_automaton_details(mealy, 'git-model.log')
+
+    clean_up()
+
+    # # Second version, imitating the behaviour of command line commit
+    # git_sul_2 = GitSUL(repo_path, bare_repo_path, True)
+    # eq_oracle_2 = RandomWordEqOracle(input_alphabet, git_sul_2, num_walks=500)
+    # mealy_2 = run_Lstar(input_alphabet, git_sul_2, eq_oracle_2, automaton_type='mealy', cache_and_non_det_check=True)
+    # # mealy = run_KV(input_alphabet, git_sul, eq_oracle, automaton_type='mealy', cache_and_non_det_check=True)
+    #
+    # visualize_automaton(mealy_2, 'git-model-2.pdf')
+    # print_automaton_details(mealy_2)
+    # log_automaton_details(mealy_2, 'git-model-2.log')
+    #
+    # clean_up()
+    #
+    # # Compare the two automata
+    # diffs = compare_automata(mealy, mealy_2)
+    # print(diffs)
 
 
 if __name__ == '__main__':
