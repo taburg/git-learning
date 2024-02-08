ee3bc30455ce0f3e3a7776cb5c457225c1e0fa9d
Edi Muskardin <edi.muskardin@silicon-austria.com>
2024-02-08 16:11:38 +0100

    add command line interface

--- utils.py
+++ utils.py
@@ -2,47 +2,45 @@ import os
 import shutil
 
 
-def create_file(path: str) -> str:
+def create_file(path: str) -> bool:
     try:
         with open(path, 'x'):
             pass
-        return "CREATE_SUCCESS"
+        return True
     except Exception:
-        return "CREATE_FAIL"
+        return False
 
 
-def change_file(path: str) -> str:
+def change_file(path: str) -> bool:
     try:
         if not os.path.isfile(path):
             raise FileNotFoundError
         with open(path, 'a') as f:
             f.write("Some text" + '\n')
-        return "WRITE_SUCCESS"
+        return True
     except Exception:
-        return "WRITE_FAIL"
+        return False
 
 
-def delete_file(path: str) -> str:
+def delete_file(path: str) -> bool:
     try:
         os.remove(path)
-        return "DELETE_SUCCESS"
+        return True
     except Exception:
-        return "DELETE_FAIL"
+        return False
 
 
 def clean_up(git_instance, repo_path, bare_repo_path):
     if git_instance:
         git_instance.repo.close()
 
-    if os.path.exists(repo_path):
-        shutil.rmtree(repo_path)
+    for git_folder in [repo_path, bare_repo_path]:
+        if os.path.exists(git_folder):
+            os.chmod(git_folder, 0o777)
+            for root, dirs, files in os.walk(git_folder):
+                for d in dirs:
+                    os.chmod(os.path.join(root, d), 0o777)
+                for f in files:
+                    os.chmod(os.path.join(root, f), 0o777)
 
-    if os.path.exists(bare_repo_path):
-        os.chmod(bare_repo_path, 0o777)
-        for root, dirs, files in os.walk(bare_repo_path):
-            for d in dirs:
-                os.chmod(os.path.join(root, d), 0o777)
-            for f in files:
-                os.chmod(os.path.join(root, f), 0o777)
-
-        shutil.rmtree(bare_repo_path)
+            shutil.rmtree(git_folder)
