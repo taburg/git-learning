81ac18236cf606232ed3d98ea582db4706b8f66b
Edi Muskardin <edi.muskardin@silicon-austria.com>
2024-02-10 17:55:27 +0100

    update comparison script

--- interface_comparison.py
+++ interface_comparison.py
@@ -22,7 +22,7 @@ for input_al_name, input_al in [('basic', basic_functionality_alphabet),
     for alg_name, learning_alg in [('L_star', run_Lstar),]:
             for allow_empty in [True, False]:
 
-                print(f'Comparing CMD and Gitpython on {input_al_name}')
+                print(f'Comparing CMD and Gitpython on {input_al_name}, allow empty {allow_empty}')
 
                 cmd_model = load_automaton_from_file(f'models/cmd_empty_{allow_empty}_{input_al_name}_{alg_name}.dot', 'mealy')
                 gitPython_model = load_automaton_from_file(f'models/gitPython_empty_{allow_empty}_{input_al_name}_{alg_name}.dot', 'mealy')
@@ -37,9 +37,9 @@ for input_al_name, input_al in [('basic', basic_functionality_alphabet),
                     more_cex = compare_automata(cmd_model, gitPython_model, num_cex=5)
                     print('Additional counterexamples')
                     for cex in more_cex:
-                        print(f'Counterexample        : {shortest_counterexample}')
-                        print(f'CFM Interface           : {cmd_model.execute_sequence(cmd_model.initial_state, shortest_counterexample)}')
-                        print(f'GitPython Interface     : {gitPython_model.execute_sequence(gitPython_model.initial_state, shortest_counterexample)}')
+                        print(f'Counterexample        : {cex}')
+                        print(f'CFM Interface           : {cmd_model.execute_sequence(cmd_model.initial_state, cex)}')
+                        print(f'GitPython Interface     : {gitPython_model.execute_sequence(gitPython_model.initial_state, cex)}')
 
                 else:
                     print("CMD and GitPython interfaces behave the same.")
